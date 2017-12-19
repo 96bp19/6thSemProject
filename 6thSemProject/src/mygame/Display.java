cae20dc0a8f1f433875c073f6f0f1d48c4ef82f5
binaya poudel <lanthoo170@gmail.com>
2017-12-19 20:25:01 +0545

    Initial Setup for Project

---
+++ 6thSemProject/src/mygame/Display.java
@@ -0,0 +1,81 @@
+package mygame;
+
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Display
+{
+	JFrame frame;
+	Canvas canvas;
+	String title;
+	int height,width;
+	
+	public  Display(String title, int width ,int height)
+	{
+		
+		this.height=700;
+		this.width=1000;
+		this.title=title;
+		
+		displayFrame();
+	}
+	
+	void displayFrame()
+	{
+		frame=new JFrame(title);
+		frame.setSize(width, height);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		canvas=new Canvas();
+		canvas.setPreferredSize(new Dimension(width,height));
+		canvas.setMinimumSize(new Dimension(width,height));
+		canvas.setMaximumSize(new Dimension(width,height));
+		
+		frame.add(canvas);
+		frame.pack();
+		
+		
+	}
+	
+	public Canvas getCanvas()
+	{
+		return canvas;
+	}
+	
+	public Image getImage(String path)
+	{
+		Image img =null;
+		
+		try {
+			//img=new ImageIcon("D:\\6thSemProject\\6thSemProject\\src\\abcd.jpg").getImage();
+			img = new ImageIcon(getClass().getResource(path)).getImage();
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("no image found");
+			System.exit(1);
+		}
+		
+		
+		
+		
+		return img;
+	}
+
+	
+	
+	
+	
+}
---
+++ 6thSemProject/src/mygame/Main.java
@@ -0,0 +1,166 @@
+package mygame;
+
+import java.awt.*;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Main implements Runnable,KeyListener {
+	
+	
+	private Graphics g;
+	private BufferStrategy bs; // used to provide good graphical display
+	private Image img=null; // used for getting image 
+	
+	Thread t;
+	boolean isRunning=false;
+	
+	
+	private Display display; // it is just the class we created 
+	public  String title;
+	public int width,height;
+	
+	public Main(String title,int width, int height)
+	{
+		
+		this.title=title;
+		this.width=width;
+		this.height=height;
+		
+		
+		display=new Display(title,width,height);
+		
+		
+			
+		
+		display.getCanvas().addKeyListener(this);
+		display.getCanvas().setFocusable(true);
+	}
+	
+	// write graphics related code here 
+	public synchronized void graphicsRenderer()
+	{
+		bs=display.getCanvas().getBufferStrategy(); // this is used to provide smooth graphics
+		
+		if(bs==null) // used so that bs value is set only once
+		{
+			display.getCanvas().createBufferStrategy(3); // 3 denotes triple buffering
+														// 2 denotes double buffering 
+													   // more than 3 causes problems
+			return;
+		}
+		
+		g=bs.getDrawGraphics(); // graphics is initialized
+		g.clearRect(0, 0, width, height); // clears whole screen
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 100, 50, 50);
+		
+		img =display.getImage("Images\\game.jpg");
+		
+		g.drawImage(img, 100, 100, 200, 200, null, null);
+		
+		bs.show();
+		
+		g.dispose();
+		
+	}
+	
+	//write main game logic here//
+	/////////////////////////////
+	public void Update()
+	{
+		
+	}
+	
+	public synchronized void start()
+	{
+		if(isRunning)
+			return;
+		isRunning=true;
+		t=new Thread(this);
+		t.start();
+	}
+	public synchronized void stop()
+	{
+		if(!isRunning)
+		return;
+		
+		isRunning=false;
+		try 
+		{
+			t.join();
+		} 
+		
+		catch (InterruptedException e) 
+		{
+			
+			e.printStackTrace();
+		}
+	}
+
+	
+	public synchronized void run()
+	{
+		
+		 int fps=60;
+		 double timePerTick=1000000000/fps;
+		 double delta=0;
+		 long now;
+		 long lastTime=System.nanoTime();
+		 while(isRunning) // this is for infinite loop with 60 fps
+		 {
+			 now =System.nanoTime();
+			 delta+=(now-lastTime)/timePerTick;
+			 lastTime=now;
+			 
+			 if(delta>=1)
+			 {
+				
+				 graphicsRenderer();
+				 Update();
+				 delta--;
+			 }
+			
+		 }
+		 
+		 stop();
+		System.out.println("hello");
+		
+	}
+	
+	public void keyPressed(KeyEvent e)
+	{
+		
+	}
+	
+	public void keyReleased(KeyEvent e) 
+	{
+		// not used 
+	}
+
+	
+	public void keyTyped(KeyEvent e)
+	{
+		// not used
+		
+	}
+	
+	
+	public static void main(String args[])
+	{
+		
+		Main obj = new Main("smash it",1000,700);
+		obj.start();
+	    //obj.run();
+		
+	}
+	
+
+}
